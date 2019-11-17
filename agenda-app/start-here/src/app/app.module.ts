75c930d3b92ac66c3cec403de601311b19b8159d
PalantirDagor <duber.alexander@gmail.com>
2019-11-17 10:01:44 -0500

    cambios realizados a todos los faltantes

--- agenda-app/start-here/src/app/app.module.ts
+++ agenda-app/start-here/src/app/app.module.ts
@@ -1,18 +1,24 @@
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { EventComponent } from './components/event/event.component'
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    EventComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MDBBootstrapModule.forRoot()
+    MDBBootstrapModule.forRoot(),
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent],
